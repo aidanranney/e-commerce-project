3d227195bdbf80874bb5afd96605174f2b9b3582
Jesse Johnston <essejjj@gmail.com>
2018-05-24 12:36:43 -0700

    Added validation for album and user fields

--- web/insertUser.php
+++ web/insertUser.php
@@ -3,19 +3,44 @@
 	<head>
 		<meta charset="utf-8">
 		<title>PHP Form Send</title>
+<script>
+function validation() {
 
+// VALIDATION CODE HERE!
+
+if(document.getElementById('USEREMAIL').value ==''){
+  alert("You must include a valid Email Address");
+  return false;
+} if(document.getElementById('firstName').value ==''){
+  alert("Please include your first name");
+  return false;
+} if(document.getElementById('lastName').value ==''){
+	alert("Please include your first name");
+	return false;
+} if(document.getElementById('DOB').value < 1998-01-01){
+  alert("You must be at least 18 years old");
+  return false;
+} if(document.getElementById('password').value.length < 4){
+  alert("Password must have at least 4 characters");
+  return false;
+} if(document.getElementById('MAILADDRESS').value ==''){
+	alert("Please include your mailing address");
+	return false;
+}
+}
+</script>
 	</head>
 
 	<body>
-<form action="setUser.php" method="POST">
-	<p>Email Address:<input type="text" name="USEREMAIL"/></p>
-	<p>First Name: <input type="text" name="firstName" /></p>
-  <p>Last Name:<input type="text" name="lastName"/></p>
-  <p>Date of Birth: <input type="date" name="DOB" /></p>
-  <p>Password<input type="text" name="password"/></p>
-  <p>Mailing Address: <input type="text" name="MAILADDRESS" /></p>
-  <p>Shippig Address:<input type="text" name="SHIPADDRESS"/></p>
-  <p>Phone Number: <input type="text" name="phoneNumber" /></p>
+<form action="setUser.php" method="POST" onsubmit="return validation();">
+	<p>Email Address:<input type="text" name="USEREMAIL" id="USEREMAIL"/></p>
+	<p>First Name: <input type="text" name="firstName" id="firstName" /></p>
+  <p>Last Name:<input type="text" name="lastName" id="lastName"/></p>
+  <p>Date of Birth: <input type="date" name="DOB" id="DOB" /></p>
+  <p>Password<input type="text" name="password" id="password"/></p>
+  <p>Mailing Address: <input type="text" name="MAILADDRESS" id="MAILADDRESS" /></p>
+  <p>Shipping Address:<input type="text" name="SHIPADDRESS" id="SHIPADDRESS"/></p>
+  <p>Phone Number: <input type="text" name="phoneNumber" id="phoneNumber" /></p>
 	<p><input type="submit" value="Submit" /></p>
 
 </form>
