071e2d36d8e6a62b4b56088103d26570ec40a70a
Aidan <aidan.s.ranney@gmail.com>
2018-05-25 18:00:48 -0700

    bootstrap products, needs format fix

--- web/index.php
+++ web/index.php
@@ -11,20 +11,35 @@ print "<div class='container'>";
  $result = mysqli_query($link,'select * from RECORD');
 
  if ($result)   {
-
-     $row_count = mysqli_num_rows($result);
-
-     print 'Retreived '. $row_count . ' rows from the <b> RECORD </b> table<BR><BR>';
-
      while ($row = mysqli_fetch_array($result)) {
-
-         print $row['itemNumber'] . '. ' . $row['artist'] . ', ' . $row['albumTitle'] .', ' .
-         $row['genre'] .', ' . ' $'. $row['PRICE'] .', ' . $row['RELEASEDATE'] .', ' . $row['quality'] . ', ' . $row['EDITIONNUMBER'] . ', ' . $row['albumArtwork'] . '<br>';
-
+         echo "
+         <div class='col-sm-3'>
+            <article class='col-item'>
+            	<div class='photo'>
+        			<div class='options-cart-round'>
+        			</div>
+        			<a href=''#''> <img src='../images/records.jpg' class='img-responsive' alt='Product Image' /> </a>
+        		</div>
+        		<div class='info'>
+        			<div class='row'>
+        				<div class='price-details col-md-6'>
+        					<p class='details'>"
+        						. $row['quality'] . "
+        					</p>
+        					<h4>" . $row['albumTitle'] . "</h4>
+                  <p class='details'><b>" . $row['artist'] . "</b></p>
+        					<span class='price-new'>" . "$" . $row['PRICE'] . "</span>
+        				</div>
+        			</div>
+        		</div>
+        	</article>
+        </div>";
+        // $row['itemNumber'] . '. ' . $row['artist'] . ', ' . $row['albumTitle'] .', ' .
+        // $row['genre'] .', ' . ' $'. $row['PRICE'] .', ' . $row['RELEASEDATE'] .', ' . $row['quality'] . ', ' . $row['EDITIONNUMBER'] . ', ' . $row['albumArtwork'] . '<br>';
      }
 
  }
- print "</div>";  
+ print "</div>";
 
  include ('footer.php');
 ?>
