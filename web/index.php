60d8a87453f42f3cdbed414c468faacc2b7922bb
Aidan <aidan.s.ranney@gmail.com>
2018-05-28 15:49:54 -0700

    modified display grid

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@ include ('header.php');
 include ('connection.php');
 ?>
 
-<div class='container'>
+<div class='container-fluid'>
 <?php
 if (isset($_GET['genre'])) {
   $genre = $_GET['genre'];
@@ -24,12 +24,11 @@ while ($row = mysqli_fetch_array($result)) {
         echo "
         <div class='col-sm-3'>
            <article class='col-item'>
-           	<div class='photo'>
-       			<a href=''#''> <img src='" . $row['albumArtwork'] . "' class='img-responsive' alt='Product Image' height=200 width=200/> </a>
+            <div class='photo'>
+       			<a href=''#''> <img src='" . $row['albumArtwork'] . "' alt='Product Image' height=200 width=200> </a>
        	    </div>
        		<div class='info'>
-       			<div class='row'>
-       				<div class='price-details col-md-9'>
+       				<div class='price-details col-md-10'>
        					<div class='details'>"
        						. $row['quality'] . "
        					</div>
@@ -40,14 +39,12 @@ while ($row = mysqli_fetch_array($result)) {
                  <br>
                  <br>
        				</div>
-       			</div>
        		</div>
        	</article>
-       </div>";
+        </div>";
 }
 ?>
-
-</div>;
+</div>
 
 <?php
  include ('footer.php');
