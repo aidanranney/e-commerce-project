d19b861a868ca1df5364c6aca934f333f4c4b7d9
Aidan <aidan.s.ranney@gmail.com>
2018-05-28 10:23:55 -0700

    ...

--- web/connection.php
+++ web/connection.php
@@ -2,20 +2,14 @@
 
 
 $server = 'localhost';
-
-$user = 'cst106';
-
-$pswd = '344994';
-
+$user = 'cst161';
+$pswd = '427021';
 $db='ICS199Group08_prod';
-
 
 $link = mysqli_connect($server,$user,$pswd,$db);
 
 if (!$link) {
-
     die ('MySQL Error:' . mysqli_connect_error());
-
 }
 
 ?>
