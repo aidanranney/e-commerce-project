1d8344977c47b2a4a631d026c60a8a2683b175e7
Andrew Bishop <andrew.bishop53@gmail.com>
2018-05-30 22:24:44 -0700

    Did some work on the back end and changed the index page SQL queries. Now all the records are showing up in the right genre categories, which can be multiple

--- web/header.php
+++ web/header.php
@@ -39,8 +39,8 @@ and open the template in the editor.
                         echo "<li id='All'><a href='index.php'>All</a></li>";
                         if ($categories)   {
                           while ($result = mysqli_fetch_array($categories)) {
-                          $id = $result['genre'];
-                          echo "<li id=" . $id . "><a href='index.php?genre=$id'>" . $id . "</a></li>";
+                          $genre = $result['genre'];
+                          echo "<li id=" . $genre . "><a href='index.php?genre=$genre'>" . $genre . "</a></li>";
                           }
                         }
 
--- web/index.php
+++ web/index.php
@@ -9,48 +9,55 @@ include ('connection.php');
 <?php
 if (isset($_GET['genre'])) {
   $genre = $_GET['genre'];
-  $query = "select * from RECORD WHERE genre ='$genre'";
-  $result = mysqli_query($link,"select * from RECORD WHERE genre ='$genre'");
+  $query = "select * from RECORD where itemNumber IN
+	   (SELECT RECORD_itemNumber from RECORD_CATEGORY where GENRE_genreID IN
+     (SELECT genreID from GENRE where genre = '$genre'))";
+  $result = mysqli_query($link, $query);
 } else {
   $query = "select * from RECORD";
-  $result = mysqli_query($link,"select * from RECORD");
+  $result = mysqli_query($link, $query);
 }
 if (isset($genre))   {
   echo "<h4>$genre Records</h4>";
 } else {
   echo "<h4>All Records</h4>";
 }
-while ($row = mysqli_fetch_array($result)) {
-        echo "
-        <div class='col-sm-3'>
-           <article class='col-item'>
-            <div class='albumArtwork'>
-       			<img src='" . $row['albumArtwork'] . "' alt='Product Image' height=200 width=200>
-            <div class='item-buttons'>
-              <div class='animated fadeInDown'>
-                <button id='itemDescription' class='btn btn-info' title='Product description'>
-                <span class='glyphicon glyphicon-plus'></span></button>
-                <button id='shoppingCart' class='btn btn-info' title='Add to cart'>
-              <span class='glyphicon glyphicon-shopping-cart'></span></button>
+if (mysqli_num_rows($result) > 0) {
+  while ($row = mysqli_fetch_array($result)) {
+          echo "
+          <div class='col-sm-3'>
+             <article class='col-item'>
+              <div class='albumArtwork'>
+         			<img src='" . $row['albumArtwork'] . "' alt='Product Image' height=200 width=200>
+              <div class='item-buttons'>
+                <div class='animated fadeInDown'>
+                  <button id='itemDescription' class='btn btn-info' title='Product description'>
+                  <span class='glyphicon glyphicon-plus'></span></button>
+                  <button id='shoppingCart' class='btn btn-info' title='Add to cart'>
+                <span class='glyphicon glyphicon-shopping-cart'></span></button>
+                </div>
+                </div>
               </div>
-              </div>
-            </div>
-       		<div class='info'>
-       				<div class='price-details col-md-10'>
-       					<div class='details'>"
-       						. $row['quality'] . "
-       					</div>
-       					<div style='font-size:16pt'>" . $row['albumTitle'] . "</div>
-                 <b>" . $row['artist'] . "</b>
-                 <br>
-       					<span class='price-new'>" . "$" . $row['PRICE'] . "</span>
-                 <br>
-                 <br>
-       				</div>
-       		</div>
-       	</article>
-        </div>";
+         		<div class='info'>
+         				<div class='price-details col-md-10'>
+         					<div class='details'>"
+         						. $row['quality'] . "
+         					</div>
+         					<div style='font-size:16pt'>" . $row['albumTitle'] . "</div>
+                   <b>" . $row['artist'] . "</b>
+                   <br>
+         					<span class='price-new'>" . "$" . $row['PRICE'] . "</span>
+                   <br>
+                   <br>
+         				</div>
+         		</div>
+         	</article>
+          </div>";
+  }
+} else {
+  echo "No records match that genre";
 }
+
 ?>
 </div>
 
