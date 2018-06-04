bd4292db313c7ebd0de88e574070b760814cf5ef
Jesse Johnston <essejjj@gmail.com>
2018-06-04 09:59:12 -0700

    Added registrationValidation script for the registration page

--- web/JS/scripts.js
+++ web/JS/scripts.js
@@ -40,6 +40,40 @@ function validateRecord() {
 }
 }
 
+function registrationValidation() {
+
+// VALIDATION CODE HERE!
+
+if(document.getElementById('USEREMAIL').value ==''){
+  alert("You must include a valid Email Address");
+  return false;
+} if(document.getElementById('firstName').value ==''){
+  alert("Please include your first name");
+  return false;
+} if(document.getElementById('lastName').value ==''){
+	alert("Please include your first name");
+	return false;
+} if(document.getElementById('DOB').value < 1998-01-01){
+  alert("You must be at least 18 years old");
+  return false;
+} if(document.getElementById('password').value.length < 4){
+  alert("Password must have at least 4 characters");
+  return false;
+} if (document.getElementById('password2').value ==''){
+  alert("Please re-enter your password");
+  return false;
+} if(document.getElementById('address').value ==''){
+	alert("Please include your address");
+	return false;
+} if(document.getElementById('city').value ==''){
+	alert("Please include your city");
+	return false;
+}if(document.getElementById('postal_code').value ==''){
+  	alert("Please include your postal code");
+  	return false;
+  }
+}
+
 function validateCart() {
 
 }
