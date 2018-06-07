b59d9ca0627e248de691076328fb4cc46a48af4a
Jesse Johnston <essejjj@gmail.com>
2018-06-07 11:25:03 -0700

    new session variable for privacy

--- web/loginconfirm.php
+++ web/loginconfirm.php
@@ -10,19 +10,29 @@ $password = $_POST['password'];
 $salted = "456y45rghtrhfgr23441ldk3".$password."32490ffsll33";
 $hashed = hash('sha1', $salted);
 //create query
+
 $query = "SELECT * from USER_ACCOUNT WHERE USEREMAIL='$useremail' AND password='$hashed'";
 $adminquery = "SELECT admin from USER_ACCOUNT WHERE USEREMAIL='$useremail' AND password='$hashed'";
+$privQuery = "SELECT privCheck from USER_ACCOUNT WHERE USEREMAIL='$useremail' AND password='$hashed'";
 //run query. the @ symbol suppresses errors
+$privacy = @mysqli_query($link, $privQuery);
 $row = @mysqli_query($link, $query);
 $admin = @mysqli_query($link, $adminquery);
 //check the result
+
 if (mysqli_num_rows($row) == 1) {
-        $addLog = "UPDATE USER_ACCOUNT SET last_login = NOW() WHERE USEREMAIL = '$useremail'";
-		mysqli_query($link, $addLog);
-	echo "<p>You have logged in successfully</p>";
+			$addLog = "UPDATE USER_ACCOUNT SET last_login = NOW() WHERE USEREMAIL = '$useremail'";
+			mysqli_query($link, $addLog);
+	echo "<p>You have logged in successfully</p>;"
+
+
+
 	$_SESSION['useremail']=$useremail;
 	$r = mysqli_fetch_array($admin);
-	$_SESSION['admin']= $r['admin'];
+	$p = mysqli_fetch_array($privacy);
+	$_SESSION['privacy'] = $p['privCheck'];
+	$_SESSION['admin'] = $r['admin'];
+	echo '$_SESSION['privacy'] $_SESSION['admin']';
 	if (isset($_GET['itemNumber'])) {
 			$item = $_GET['itemNumber'];
 			$addItem = "INSERT INTO SHOPPING_CART (quantityOrdered, RECORD_itemNumber, USER_ACCOUNT_USEREMAIL)
