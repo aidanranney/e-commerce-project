6a0f3a40ccd148557694d7ac4c68d1f4538313a8
Matthew Singleton <matthewsflux@gmail.com>
2018-06-13 11:58:43 -0700

    Fixed being able to change terms of service while not logged in. Which caused an error to occur when accepted

--- web/footer.php
+++ web/footer.php
@@ -19,7 +19,11 @@
          </div>
          <button type="submit" class="btn btn-info">Subscribe</button>
      </form>
-          <a href='index.php?privacy=N' button type"submit" class="btn btn-info">Change Terms of Use</a>
+		<?php
+		if(isset($_SESSION['useremail'])){
+			echo "<a href='index.php?privacy=N' button type'submit' class='btn btn-info'>Change Terms of Use</a>";
+		}
+		?>
    </div>
    <div id='mid-footer' class="col-sm-4 footer-section">
      <ul style="list-style-type:none">
